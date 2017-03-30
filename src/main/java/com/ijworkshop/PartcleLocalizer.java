336356d94bbb4a1a33deec4222ec9cb156acf0c1
Ko Sugawara <sugawara@lpixel.net>
2017-03-30 13:11:31 +0900

    initial commit

---
+++ src/main/java/com/ijworkshop/PartcleLocalizer.java
@@ -0,0 +1,21 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package com.ijworkshop;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class PartcleLocalizer implements PlugIn {
+
+	@Override
+	public void run(String arg0) {
+		IJ.log("Hello world2");
+	}
+	
+}
